35f07959f9d05026c6450db621187ec45d5dd9fd
Oleh Senkiv <33096162+senkiv-oleh@users.noreply.github.com>
2025-04-06 23:19:23 +0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_PATH: string
+  readonly REACT_APP_API_URL: string
   readonly VITE_GMAIL_CLIENT_ID: string
   readonly VITE_APP_IMG_URL: string
   readonly VITE_APP_IMG_USER_URL: string
