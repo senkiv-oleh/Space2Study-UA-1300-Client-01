649a667d957f84d2828ce549110ed799c947cdf4
Oleh Senkiv <33096162+senkiv-oleh@users.noreply.github.com>
2025-04-07 12:48:34 +0300

    Update apiSlice.ts

--- src/redux/apiSlice.ts
+++ src/redux/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const appApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.envREACT_APP_API_URL,
+    baseUrl: import.meta.env.REACT_APP_API_URL,
     credentials: 'include'
   }),
   reducerPath: 'appApi',
